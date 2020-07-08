23b97172d5154838a9652af84594e50563ebfc50
ShubhamTGC <67779450+ShubhamTGC@users.noreply.github.com>
2020-07-09 00:29:54 +0530

    worked 8 july
    diy page update

---
+++ TestWasteManagement/Assets/Scripts/DIYpageHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIYpageHandler : MonoBehaviour
+{
+    // Start is called before the first frame updateNext
+    public GameObject NextButton, BackButton;
+    public List<GameObject> Pagelist;
+    private int pagecounter=0;
+    private int lastpagecounter=0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pagecounter == 0)
+        {
+            BackButton.SetActive(false);
+            NextButton.SetActive(true);
+        }
+        else if(pagecounter >0 && pagecounter < Pagelist.Count - 1)
+        {
+            BackButton.SetActive(true);
+            NextButton.SetActive(true);
+        }
+        else if(pagecounter < Pagelist.Count)
+        {
+            NextButton.SetActive(false);
+        }
+    }
+
+    public void NextpageEnable()
+    {
+        lastpagecounter = pagecounter;
+        pagecounter++;
+        Pagelist[pagecounter].SetActive(true);
+        Pagelist[lastpagecounter].SetActive(false);
+    }
+
+    void Enablepages()
+    {
+
+    }
+    public void BackPageEnable()
+    {
+        lastpagecounter = pagecounter;
+        pagecounter--;
+        Pagelist[pagecounter].SetActive(true);
+        Pagelist[lastpagecounter].SetActive(false);
+    }
+}
